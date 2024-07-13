c47a2e88c9f379a0e0c1868d1dcfe991902e9648
Samir DERKAOUI <126152143+Kaiser016X@users.noreply.github.com>
2024-07-13 14:11:47 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,15 @@
+module "aws_vpc" {
+  source                      = "terraform-aws-modules/vpc/aws"
+
+  name                        = var.vpc_name_description[terraform.workspace]
+  cidr                        = "10.0.0.0/16"
+
+  azs                         = ["eu-west-3a", "eu-west-3b"]
+  private_subnets             = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets              = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway          = true
+  enable_vpn_gateway          = true
+  enable_dns_hostnames        = true
+  enable_dns_support          = true
+}
