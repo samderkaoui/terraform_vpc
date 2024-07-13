c389d3cdc6eda00bd25f0e0e61be09790add824a
Samir DERKAOUI <126152143+Kaiser016X@users.noreply.github.com>
2024-07-13 14:22:55 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,8 +1,4 @@
 variable "vpc_name_description" {
   description = "VPC_Test"
-  type        = map(string)
-  default     = {
-    prod      = "VPC_Production"
-    test      = "VPC_Test"
-  }
+  type        = string
 }
