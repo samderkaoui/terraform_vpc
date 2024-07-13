417be95e0630d02d0a7a20aa0978ee09a6c3c91d
Samir DERKAOUI <126152143+Kaiser016X@users.noreply.github.com>
2024-07-13 14:15:04 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,11 +2,11 @@ module "aws_vpc" {
   source                      = "terraform-aws-modules/vpc/aws"
 
   name                        = var.vpc_name_description[terraform.workspace]
-  cidr                        = "10.0.0.0/16"
+  cidr                        = "10.10.0.0/16"
 
   azs                         = ["eu-west-3a", "eu-west-3b"]
-  private_subnets             = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets              = ["10.0.101.0/24", "10.0.102.0/24"]
+  private_subnets             = ["10.10.1.0/24", "10.10.2.0/24"]
+  public_subnets              = ["10.10.101.0/24", "10.10.102.0/24"]
 
   enable_nat_gateway          = true
   enable_vpn_gateway          = true
